d201eb0329211c70938d40535a0514fd682772b5
naveen sanjula <82176749+naveensanjula975@users.noreply.github.com>
2025-07-04 17:09:20 +0530

    Add GitHub Pages deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/my-app' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-app' : '',
 };
 
 export default nextConfig;
